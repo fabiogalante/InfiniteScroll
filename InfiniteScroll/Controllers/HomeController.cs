f8aff72007a6a87b003d1990a853f5170783a162
devmediatestegits <devmedia2014@outlook.com>
2015-04-02 17:29:08 -0300

    Infinite

---
+++ InfiniteScroll/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using InfiniteScroll.Models;
+
+namespace InfiniteScroll.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult ObterProdutos(int ultimoRegistro)
+        {
+            List<MarcaProdutoCategoria> produtos;
+            using (var context = new EfDbContext())
+            {
+                produtos = context.Produtos
+                    .OrderBy(p => p.Id)
+                    .Skip((ultimoRegistro - 1)*10)
+                    .Take(10).ToList();
+            }
+
+
+            return Json(new { Result = produtos }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ InfiniteScroll/Models/MarcaProdutoCategoria.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace InfiniteScroll.Models
+{
+    public class MarcaProdutoCategoria
+    {
+        public string Id { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Produto { get; set; }
+
+        public string Categoria { get; set; }
+
+        public string Caracteristica { get; set; }
+
+        public string Caminho { get; set; }
+    }
+
+    public class EfDbContext : DbContext
+    {
+        public DbSet<MarcaProdutoCategoria> Produtos { get; set; }
+       
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<MarcaProdutoCategoria>().ToTable("MarcaProdutoCategoria");
+            //modelBuilder.Entity<Administrador>().ToTable("Administradores");
+        }
+
+    }
+}
